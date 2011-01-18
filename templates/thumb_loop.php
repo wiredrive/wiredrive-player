0c962e2b2af575c9ead9cf7b42f60f17b8422e58
Drew Baker <drew@wiredrive.com>
2011-01-17 17:31:30 -0800

    Changed H3 and H4 in credits to SPAN tags

--- templates/thumb_loop.php
+++ templates/thumb_loop.php
@@ -10,8 +10,8 @@
     <ul class='wd-credits'>
         <?php foreach ( $this->get('items') as $key=>$item ) : ?>   
         <li rel="<?php echo $key ?>">
-            <h3 class='wd-title'><?php echo $item['title'] ?></h3>
-            <h4 class='wd-credit'><?php echo $item['credit'] ?></h4>    
+            <span class='wd-title'><?php echo $item['title'] ?></span>
+            <span class='wd-credit'><?php echo $item['credit'] ?></span>    
         </li>
         <?php endforeach; ?>
     </ul>
