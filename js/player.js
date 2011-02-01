c0a8962d1576a1e849ae147c0ebc7883050e406f
Drew Baker <drew@wiredrive.com>
2011-01-31 17:25:09 -0800

    Added resize function into main player.js file

--- js/player.js
+++ js/player.js
@@ -4,6 +4,24 @@
  */
     
     VideoJS.setupAllWhenReady();
+    
+    // The resizing function for slideshow images.    
+    function fit_within_box(box_width, box_height, new_width, new_height)
+    {
+        var aspect_ratio=new_width/new_height;
+        if(new_width>box_width){
+            new_width=box_width;
+            new_height=Math.round(new_width/aspect_ratio);
+        }
+        if(new_height>box_height){
+            new_height=box_height;
+            new_width=Math.round(new_height*aspect_ratio);
+        }    
+        return {
+            width: new_width, 
+            height: new_height
+        };
+    };
                 
     jQuery(document).ready(function($) {
         
