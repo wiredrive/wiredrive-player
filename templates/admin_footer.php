73af85ff10d64d72ce83505ea247348edc4e6e4d
Drew Baker <drew@wiredrive.com>
2011-02-09 17:21:49 -0800

    Fixed spelling on dialog boxes

--- templates/admin_footer.php
+++ templates/admin_footer.php
@@ -8,15 +8,15 @@
             </div>
             <p><input type="text" id="wdp-dialog-feed" style="width:98%" /></p>
             <p><strong>Set dimensions for your player:</strong></p>
-            <p>YIf left blank, the player defaults to the maximum width of your page by 480 pixels high.</p>
+            <p>If left blank, the player defaults to the maximum width of your page by 480 pixels high.</p>
             <p><input type="text" id="wdp-dialog-width" class="wdp-dialog-dim" style="width:50px" /> &#215; 
                <input type="text" id="wdp-dialog-height" class="wdp-dialog-dim" style="width:50px" /> pixels</p>
             <p>[width] &#215; [height]</p>
             <p><strong>Options:</strong></p>
-            <p><input type="checkbox" id="wdp-hide-thumbs"/> Hide thumbnail tray.</p>
-            <p><input type="checkbox" id="wdp-auto-slideshow"/> Slideshow mode (gallery feeds only).</p>
-            <p>
-                <input type="radio" id="wdp-theme-default" name="theme" value="default" checked/>Dark<br />
+            <p><input type="checkbox" id="wdp-hide-thumbs"/> Hide thumbnail tray<br />
+               <input type="checkbox" id="wdp-auto-slideshow"/> Slideshow mode (gallery feeds only)</p>
+            <p><strong>Thumbnail tray background color:</strong></p>
+                <input type="radio" id="wdp-theme-default" name="theme" value="default" checked/> Dark<br />
                 <input type="radio" id="wdp-theme-2" name="theme" value="theme2" /> Gray<br />
                 <input type="radio" id="wdp-theme-3" name="theme" value="theme3" /> White<br />
             </p>
