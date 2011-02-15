6401b37068d9d2d9d176233686042a680754c876
Daniel Bondurant <bondu@wiredrive.com>
2011-02-15 10:09:32 -0800

    formating css a bit to avoid line unnecessary line feeds

--- templates/player_start.php
+++ templates/player_start.php
@@ -1,12 +1,14 @@
 <!-- Begin Wiredrive Player -->
-<div class='wd-player
-    <?php if ($this->get('hidethumbs') == 'on') : ?> hide-thumbs<?php endif; ?> 
-    <?php if ($this->get('mobile') == true) : ?> mobile<?php else : ?> not-mobile<?php endif; ?> 
-    <?php if ($this->get('slideshow') == 1) : ?>slideshow<?php else : ?>not-slideshow<?php endif; ?> 
-    <?php if ($this->get('autoslideshow') == 'on') : ?>autoslideshow<?php else : ?>not-autoslideshow<?php endif; ?> 
-    <?php echo $this->get('theme') ?> 
-    ' style='width:<?php echo $this->get('width') ?>;' >
-<div class='wd-stage' 
-        data-wd-item="0" 
-        style='height:<?php echo $this->get('height') ?>; width:<?php echo $this->get('width') ?>; 
-        '>
+<div class='wd-player<?php 
+        if ($this->get('hidethumbs') == 'on') : ?>hide-thumbs<?php endif; 
+        echo ' ';
+        if ($this->get('mobile') == true) : ?> mobile<?php else : ?> not-mobile<?php endif;
+        echo ' ';
+        if ($this->get('slideshow') == 1) : ?>slideshow<?php else : ?>not-slideshow<?php endif;  
+        echo ' ';
+        if ($this->get('autoslideshow') == 'on') : ?>autoslideshow<?php else : ?>not-autoslideshow<?php endif; 
+        echo ' ';
+        echo $this->get('theme') 
+        ?>' style='width:<?php echo $this->get('width') ?>' >
+<div class='wd-stage' data-wd-item='0' style='height:<?php 
+        echo $this->get('height') ?>;width:<?php echo $this->get('width') ?>;'>
