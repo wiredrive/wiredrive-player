6ed43a6411bb3c3ed22b3da3422fbea1827cb14e
Drew Baker <drew@wiredrive.com>
2011-03-08 11:12:03 -0800

    Fexied wdp_height missing var

--- wiredrive.php
+++ wiredrive.php
@@ -124,8 +124,8 @@ class Wiredrive_Plugin
          */
 		$options = get_option('wdp_options');
 		extract(shortcode_atts(array(
-					'height'            => $options['wdp_player_height'],
-					'width'             => $options['wdp_player_width'],
+					'height'            => $options['wdp_height'],
+					'width'             => $options['wdp_width'],
 					'hidethumbs'        => 'off',
 					'autoslideshow'     => 'off',
 					'theme'             => 'player'
