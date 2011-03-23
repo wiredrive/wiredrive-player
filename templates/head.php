1c0c5c93db33000dd29988d4becc7d7fede71521
Daniel Bondurant <bondu@wiredrive.com>
2011-03-23 10:19:25 -0700

    removing wdp from options

--- templates/head.php
+++ templates/head.php
@@ -3,42 +3,42 @@ $options = $this->get('options');
 ?>
 <style type="text/css">
 .wd-stage {
-    width: <?php echo $options['wdp_width'] . 'px' ?>;
-    height: <?php echo $options['wdp_height'] . 'px'?>;
-    background: <?php echo $options['wdp_stage_color'] ?>;
+    width: <?php echo $options['width'] . 'px' ?>;
+    height: <?php echo $options['height'] . 'px'?>;
+    background: <?php echo $options['stage_color'] ?>;
 }
 .wd-credits-container {
-    background: <?php echo $options['wdp_credit_container_color'] ?>;
-    border-top: 1px solid <?php echo $options['wdp_credit_container_border'] ?>;
-    text-align: <?php echo $options['wdp_credit_container_alignment'] ?>;
+    background: <?php echo $options['credit_container_color'] ?>;
+    border-top: 1px solid <?php echo $options['credit_container_border'] ?>;
+    text-align: <?php echo $options['credit_container_alignment'] ?>;
 }
-<?php if( $options['wdp_credit_container_alignment'] == 'Right') : ?>
+<?php if( $options['credit_container_alignment'] == 'Right') : ?>
 .hide-thumbs .wd-credits-container {
     padding-right: 35px;
 }
 <?php endif; ?>
 .wd-nav-prev, .wd-nav-next, .wd-thumb-tray {
-    background: <?php echo $options['wdp_thumb_bg_color'] ?>;
+    background: <?php echo $options['thumb_bg_color'] ?>;
 }
 .wd-left-arrow {
-    border-color: transparent <?php echo $options['wdp_arrow_color'] ?> transparent transparent;
+    border-color: transparent <?php echo $options['arrow_color'] ?> transparent transparent;
 }
 .wd-right-arrow { 
-    border-color: transparent transparent transparent <?php echo $options['wdp_arrow_color'] ?>;
+    border-color: transparent transparent transparent <?php echo $options['arrow_color'] ?>;
 }
 .wd-thumb-list .wd-active {
-    border: 1px solid <?php echo $options['wdp_active_item_color'] ?>;
+    border: 1px solid <?php echo $options['active_item_color'] ?>;
 }
 .wd-title {
-    color: <?php echo $options['wdp_title_color'] ?>;
-    font-size: <?php echo $options['wdp_title_font_size'] . 'px' ?>;
+    color: <?php echo $options['title_color'] ?>;
+    font-size: <?php echo $options['title_font_size'] . 'px' ?>;
 }
 .wd-credit {
-    color: <?php echo $options['wdp_credit_color'] ?>;
-    font-size: <?php echo $options['wdp_credit_font_size'] . 'px' ?>;
+    color: <?php echo $options['credit_color'] ?>;
+    font-size: <?php echo $options['credit_font_size'] . 'px' ?>;
 }
 .box-thumbs .wd-thumb-list li a {
     background-color: #111111;
-    background: rgba(17, 17, 17, <?php echo $options['wdp_thumb_box_opacity'] ?>);
+    background: rgba(17, 17, 17, <?php echo $options['thumb_box_opacity'] ?>);
 }
 </style>
