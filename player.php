85afe0e13887f8981e0e396a3fc56aedb7d4c110
Daniel Bondurant <bondu@wiredrive.com>
2013-01-29 11:28:18 -0800

    bumping version to 2.2.4

--- player.php
+++ player.php
@@ -4,7 +4,7 @@ Plugin Name: Wiredrive Player
 Plugin URI: http://wordpress.org/extend/plugins/wiredrive-player/
 Description: This plug-in allows you to embed Wiredrive video reels and image galleries into your WordPress-powered website.
 Author: Wiredrive
-Version: 2.2.3
+Version: 2.2.4
 Author URI: http://www.wiredrive.com/
 */
 
