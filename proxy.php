b317b75b0e3893810185db180a8bdfde21347cb0
Joshua Lai <joshua@wiredrive.com>
2013-03-20 11:52:45 -0700

    adding back empty proxy

---
+++ proxy.php
@@ -0,0 +1,9 @@
+/*
+ * this file is deprecated
+ * este archivo es obsoleto
+ * αυτό το αρχείο χρησιμοποιείται
+ * ce fichier est obsolète 
+ * Diese Datei ist veraltet.
+ * Questo file è deprecato
+ * Este arquivo é substituído
+ */
