f18b433fefe5fc30e5e6e08a9f96b88427ba524c
Aleks Drogobetski <drogobetski@users.noreply.github.com>
2016-10-24 16:00:42 -0700

    Update wd-mce-button.js

--- js/wd-mce-button.js
+++ js/wd-mce-button.js
@@ -34,7 +34,7 @@
                 author: 'Wiredrive',
                 authorurl: 'http://wiredrive.com',
                 infourl: 'http://wiredrive.com',
-                version: "3.0.3"
+                version: "3.1.0"
             };
         }
     });
